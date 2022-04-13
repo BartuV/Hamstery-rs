7944eb15609f7621c2a35bb0bc301be729864b2a
BuildTools <unconfigured@null.spigotmc.org>
2022-04-13 17:35:19 +0300

    first

---
+++ src/create.rs
@@ -0,0 +1,87 @@
+/*
+    Made by BartuV
+    github: https://github.com/BartuV
+*/
+use serde_json::*;
+use colored::*;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::str::FromStr;
+use std::{io, fs};
+
+//Source of this function: https://stackoverflow.com/questions/26958489/how-to-copy-a-folder-recursively-in-rust
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            if entry.file_name() == "pack.mcmeta"{
+                continue
+            }else {
+                fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn create(packversion:i32, location:String,packname:String){
+    let path = Path::new(&location);
+    let example = Path::new(r"src\examplepack");
+    let copyres = copy_dir_all(example, path);
+    if copyres.is_ok(){println!("{} {}", "Sucsessfully copied to".to_owned().green().bold() , &location.green().bold());}else{println!("{} {} {}","Failed to copy".to_owned().red().bold().to_string() ,&location.red().bold(),format!("{:?}",copyres.err()).red().bold());}    
+
+    let packmeta = path.as_os_str().to_str().unwrap().to_string();
+    edit_mcpack(&packmeta, packversion, &packname);
+    rename_pack(&location, &packname.to_string());
+    generate_json(&location, "tick.json".to_string(), &packname,r#"{"values": "place_holder:tick"}"#.to_string());
+    generate_json(&location, "load.json".to_string(), &packname,r#"{"values": "place_holder:load"}"#.to_string());
+}
+
+//generatin pack.mcmeta
+fn edit_mcpack(filepath:&String,version:i32,description:&String){
+    //Spesifying content of pack.mcmeta
+    let content = r#"
+    {
+        "pack": {
+          "pack_format": 9,
+          "description": "Tutorial Data Pack"
+        }
+    }
+    "#;
+
+    //Reading the content and editing it
+    let mut res = serde_json::Value::from_str(content).expect("Can't read the file");
+    let mut map = serde_json::Map::new();
+    map.insert("pack_format".to_string(), json!(version));
+    map.insert("description".to_string(), json!(description));
+    res["pack"] = serde_json::Value::Object(map);
+
+    //creating and writing to the file
+    let mut file = File::create(filepath.to_string()+&r"\pack.mcmeta".to_string()).expect("Error");
+    file.write_all(res.to_string().as_bytes()).expect("Error");
+    
+}
+
+fn rename_pack(filepath: &String,packname:&String){
+    let placeholderpath = filepath.to_string()+r"\data\place_holder";
+
+    //Renaming the file and getting the result
+    let res = fs::rename(placeholderpath, filepath.to_string()+r"\data\"+&packname.to_string());
+    if res.is_err(){println!("{} {}","Something gone wrong.".red().bold(),format!("{:?}",res.err()).red().bold())}
+}
+
+//generating file
+fn generate_json(path:&String,filename:String,name:&String,content:String){
+    //Spagetti mess but it works!
+    let mut file = File::create(path.to_string()+&r"\data\minecraft\tags\functions\".to_string()+&filename.to_string()).expect("Something went wrong.");
+    let mut res1 = Value::from_str(&content).expect("Oops. Something went wrong.");
+
+    //Editing and generating the file
+    res1["values"] = Value::Array(vec![Value::String(res1["values"].to_string().replace('"', "").replace("place_holder",&name))]);
+    file.write_all(res1.to_string().replace(r"\","").as_bytes()).expect(format!("{}","Something went wrong.".red().bold()).as_str()); 
+}
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+/*
+    Made by BartuV
+    github: https://github.com/BartuV
+*/
+#[allow(dead_code)]
+mod lexpars;
+mod create;
+use colored::*;
+use std::env;
+use std::io;
+use std::path::Path;
+/*
+commands
+
+help:    shows how to use commands
+compile: compiles to minecraft datapack
+create:  creates a example datapack
+*/
+fn main() {
+    let args:Vec<String> = env::args().collect();
+
+    if args.len() < 2{
+        error("Invalid number of arguments")
+    }else{
+        if args[1] == "help"{
+            println!("\n{}    {}\n{} {}\n{}  {}","help:".bold().green(),"shows this text","compile:".bold().green(),"compiles to minecraft datapack","create:".bold().green(),"create a example datapack")
+        }else if args[1] == "compile"{
+            let _location = get_input("Enter a pack location> ");
+            if is_valid_path(&_location){
+                lexpars::run(_location);
+            }else{
+                error("Invalid path.");
+                return
+            }
+        }else if args[1] == "create"{
+            let packname = get_input("Enter a pack name> ");
+            let packversion = get_input("Enter a pack version> ");
+            let _location = get_input("Enter a pack location> ");
+            let num:i32 =  packversion.parse().unwrap();
+            if is_valid_path(&_location){
+                create::create(num, _location, packname); 
+            }else{
+                error("Invalid path.");
+                return
+            }
+        }
+    }
+}
+
+fn error(text: &str) {
+    println!("{}{}","Command Line Error:".bold().red(), text.bold().red());
+    std::process::exit(1);
+}
+
+fn get_input(prompt: &str) -> String{
+    println!("{}",prompt);
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_goes_into_input_above) => {},
+        Err(_no_updates_is_fine) => {},
+    }
+    input.trim().to_string()
+}
+
+fn is_valid_path(path:&String) -> bool {
+    Path::new(&path).exists()
+}
